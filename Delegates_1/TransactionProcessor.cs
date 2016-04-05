6d1cf1f8fcc191bda96033fc8c93beddd660ef8e
Mikhail Ivchenko <ematirov@gmail.com>
2016-04-05 17:04:13 +0400

    Initial commit

---
+++ Delegates_1/TransactionProcessor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Delegates_1
+{
+    public abstract class TransactionProcessor
+    {
+        public Transaction Process(TransactionRequest request)
+        {
+            if (!Check(request))
+                throw new ArgumentException();
+            var result = Register(request);
+            Save(result);
+            return result;
+        }
+
+        protected abstract bool Check(TransactionRequest request);
+        protected abstract Transaction Register(TransactionRequest request);
+        protected abstract void Save(Transaction transaction);
+    }
+}
